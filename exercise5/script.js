30553eb00e6dd4a2ddbc605e47e2c07ef7713481
xuebijuly <161468413+xuebijuly@users.noreply.github.com>
2024-03-31 17:51:29 +0800

    Add files via upload

---
+++ exercise5/script.js
@@ -0,0 +1,12 @@
+document.write("hi")
+function sayHello(){
+  var response=prompt("what is your name");
+  alert("hi"+response+",have a nice day!");
+};
+//sayHello();
+
+function showTopic(){
+  var x=document.getElementById('demo');
+  x.style.fontSize="25px";
+  x.style.color="red";
+};
